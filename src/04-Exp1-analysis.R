1ac6b0b50f8c32b6eaeb2a284871e464d599af52
Brandon Edwards <brandon.edwards144@gmail.com>
2022-03-11 09:57:09 -0500

    adding more performance metrics and plotting

--- src/04-Exp1-analysis.R
+++ src/04-Exp1-analysis.R
@@ -12,13 +12,21 @@ library(ggpubr)
 library(viridis)
 theme_set(theme_pubclean())
 
+####### Set Constants #############################
+
+phi_true <- 0.4
+tau_true <- 100
+
 ####### Read Data #################################
 
 ext_data <- read.csv("output/exp1/extracted_data.csv")
 
 ####### Clean Data ################################
 
-# Remove erronous data
+# Remove erronous data but save to data frame for further inspection
+error_data <- ext_data[which(ext_data$Phi_Est < 0.001), ]
+error_data <- rbind(error_data, ext_data[which(ext_data$Tau_Est > 1000), ])
+
 ext_data <- ext_data[-which(ext_data$Phi_Est < 0.001), ]
 ext_data <- ext_data[-which(ext_data$Tau_Est > 1000), ]
 
@@ -26,13 +34,14 @@ ext_data <- ext_data[-which(ext_data$Tau_Est > 1000), ]
 ext_data$Sample_Size <- factor(ext_data$Sample_Size,
                                levels = c("75", "500", "1000"))
 
-####### Plot the Data #############################
+####### Exploratory Plots #########################
+
+  ## Univariate Plots ############
 
 phi_boxplot <- ggplot(data = ext_data,
-                   aes(x = Sample_Size, y = Phi_Est)) +
-  geom_point(aes(group = Model, color = Model),
-             position = position_dodge(width = 0.3)) +
-  geom_hline(yintercept = 0.40, linetype="dashed", color = "red") +
+                      aes(x = Sample_Size, y = Phi_Est)) +
+  geom_boxplot(aes(fill = Model)) +
+  geom_hline(yintercept = phi_true, linetype="dashed", color = "red") +
   xlab("Sample Size") + 
   ylab("Cue Rate Estimate") +
   scale_fill_viridis(discrete = TRUE) +
@@ -41,7 +50,19 @@ phi_boxplot <- ggplot(data = ext_data,
 tau_boxplot <- ggplot(data = ext_data,
                       aes(x = Sample_Size, y = Tau_Est)) +
   geom_boxplot(aes(fill = Model)) +
+  geom_hline(yintercept = tau_true, linetype="dashed", color = "red") +
   xlab("Sample Size") + 
   ylab("EDR Estimate") +
   scale_fill_viridis(discrete = TRUE) +
   NULL
+
+ ### Bivariate plots #######
+
+
+
+####### Calculate Performance Measures ############
+
+# Bias
+
+
+
